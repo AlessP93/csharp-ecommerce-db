ad873c685e5a8ead546e00b0c42454e4255d9e3c
AlessP93 <alespiscino@libero.it>
2022-09-30 16:45:51 +0200

    aggiunte crud

--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
-//using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
+Console.WriteLine("");
+
+using (EcommerceContext db = new EcommerceContext())
+{
+    // inizio CRUD - CREATE
+    Product newProduct = new Product { Name = "Iphone" , Description = "Pro 13 Pro Max 512gb", Price = "1489 €"};
+    db.Add(newProduct);
+
+    Customer newCustomer = new Customer { Name = "William Scott", Surname = "Pro 13 Pro Max 512gb", Email = "WillScott@gmail.it"};
+    db.Add(newCustomer);
+
+
+    // CRUD - READ
+    Console.WriteLine("Recupero lista di Products");
+    List<Product> Products = db.Products.OrderBy(Product => Product.Name).ToList<Product>();
+
+    Console.WriteLine("Recupero lista di Customers");
+    List<Customer> Customers = db.Customers.OrderBy(Customer => Customer.Name).ToList<Customer>();
+
+    //CRUD UPDATE
+    newProduct.Name = "Samsung";
+    db.SaveChanges();
+
+}
 public class EcommerceContext : DbContext 
 {
     public DbSet<Customer> Customers { get; set; }
